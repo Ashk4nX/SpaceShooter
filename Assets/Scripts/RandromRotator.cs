3945ad9738007c6d6baa16522ef917b1c1dd9cf2
Ashkan Abedi <ashkan.abedi@hotmail.com>
2017-08-19 19:04:09 +0430

    Commit 8/19/2017_7:04PM

--- Assets/Scripts/RandromRotator.cs
+++ Assets/Scripts/RandromRotator.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class RandromRotator : MonoBehaviour {
 
 	private Rigidbody body;
-	public float tumble;
+	private float tumble;
 
 	// Use this for initialization
 	void Start () {
 
+		tumble = Random.Range (1f, 5f);
 		body = gameObject.GetComponent<Rigidbody>();
 		body.angularVelocity = Random.insideUnitSphere * tumble;
-		body.velocity = new Vector3 (Random.Range (0.5f, 0.5f), 0.0f, Random.Range (-2f, -6f));
+		body.velocity = new Vector3 (Random.Range (0.5f, 0.5f), 0.0f, Random.Range (-2f, -4f));
 		
 	}
 	
